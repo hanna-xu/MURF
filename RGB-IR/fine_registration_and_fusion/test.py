b573fd3522e4a7c45f5132f3e23612cd86dd4edf
Han Xu <52267740+hanna-xu@users.noreply.github.com>
2023-11-10 20:00:28 +0800

    Update test.py

--- RGB-IR/fine_registration_and_fusion/test.py
+++ RGB-IR/fine_registration_and_fusion/test.py
@@ -88,10 +88,9 @@ def main():
 			FEED_DICT = {SOURCE_RGB: rgb_img_N, SOURCE_IR: ir_img_N, DEFOR_FIELD: defor_field,
 						 RE_DEFOR_FIELD_GT: defor_re}
 
-			fused_img = sess.run(f2m_model.fused_img, feed_dict=FEED_DICT)
-			fused_img = scipy.misc.imresize(fused_img[0, :, :, :], (rgb_dimension[0], rgb_dimension[1])).astype(
-				np.float32) / 255.0
-			scipy.misc.imsave(save_path + 'fused_img/' + name + '.' + format, fused_img)
+			fused_img_out = sess.run(f2m_model.fused_img, feed_dict=FEED_DICT)
+			fused_img_ori_size = cv2.resize(fused_img_out,(rgb_dimension[1], rgb_dimension[0]))
+			scipy.misc.toimage(fused_img_ori_size, cmin=0.0, cmax=np.max(fused_img_ori_size)).save(save_path + 'fused_img/' + name + '.'+format)
 
 			time = datetime.now() - start_time
 			time=time.total_seconds()
